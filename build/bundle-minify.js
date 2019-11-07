4bd9bdb236f5db9154b9a2142a52fa5c37d7a34d
chrobles <chrobles@microsoft.com>
2019-11-07 10:13:41 -0800

    fix mseutils for <es6 compat, add bundle-minify.js build script, update npm build command

--- build/bundle-minify.js
+++ build/bundle-minify.js
@@ -2,16 +2,14 @@ let uglify = require('uglify-js');
 let fs = require('fs');
 
 let dir = 'src/';
-let outdir = dir + 'static/';
-let outfile = outdir + 'bundle.min.js';
+let outdir = dir + 'static/js/';
+let bundlefile = outdir + 'bundle.min.js';
 let omit = ['ampPlayer.js'];
-
 
 /**
  * Minify source
  */
 function minifySrc() {
-  // bundle and minify all non-omitted source files 
   fs.readdir(dir, (err, filenames) => {
     if (err) {
       console.log(err);
@@ -19,12 +17,19 @@ function minifySrc() {
     let filemap = {};
     filenames.forEach(filename => {
       // don't try to read directories
+      // don't read omitted files
       if (!fs.lstatSync(dir + filename).isDirectory() && !omit.includes(filename)) {
         filemap[filename] = fs.readFileSync(dir + filename, 'utf8')
       }
     });
-    console.log(uglify.minify(filemap))
-    //fs.writeFileSync(outfile, uglify.minify(filemap).code, 'utf8');
+    // bundle and minify all non-omitted source files
+    fs.writeFileSync(bundlefile, uglify.minify(filemap).code, 'utf8');
+  });
+  omit.forEach(filename => {
+    // minify bundle omitted source files
+    fileobj = { filename: fs.readFileSync(dir + filename, 'utf8') };
+    let outfile = outdir + filename.replace(".js", ".min.js");
+    fs.writeFileSync(outfile, uglify.minify(fileobj).code, 'utf8');
   });
 }
 
